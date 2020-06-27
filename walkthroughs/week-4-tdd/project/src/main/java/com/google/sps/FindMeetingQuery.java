a9eec913721b314ef33032faae66dfcf3731ab94
johannagwan <johannagwan@gmail.com>
2020-06-28 04:40:49 +0800

    solve until test case eventSplitsRestriction

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,117 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    int meetingDuration = (int) request.getDuration();
+    if (meetingDuration > TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    if (events.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    List<TimeRange> slotsAvailable = new ArrayList<>();
+    List<Event> eventsList = new ArrayList<>(events);
+    List<Event> updatedEvents = new ArrayList<>();
+    
+    Collections.sort(eventsList, new Comparator<Event>() {
+      @Override
+      public int compare(Event a, Event b) {
+        return Long.compare(a.getWhen().start(), b.getWhen().start());
+      }
+    });
+
+    // Add earliest VALID event to updatedEvents first.
+    int firstEventNum = 0;
+    boolean findingFirstEvent = true;
+    Collection<String> meetingAttendees = request.getAttendees();
+
+    while (findingFirstEvent && firstEventNum < events.size()) {
+      Event event = eventsList.get(firstEventNum);
+      Collection<String> eventAttendees = event.getAttendees();
+
+      // If every event attendees is NOT a meeting attendee, ignore the event.
+      if (!meetingAttendees.containsAll(eventAttendees)) {
+        firstEventNum++;
+        continue;
+      }
+
+      firstEventNum++;
+      updatedEvents.add(event);
+      findingFirstEvent = false;
+    }
+
+    int updatedEventsIdx = 0;
+    for (int i = firstEventNum; i < events.size(); i++) {
+      Event event = eventsList.get(i);
+      Collection<String> eventAttendees = event.getAttendees();
+
+      // If every event attendees is NOT a meeting attendee, ignore the event.
+      if (!meetingAttendees.contains(eventAttendees)) {
+        continue;
+      }
+
+      // Check for overlaps.
+      Event prevEvent = updatedEvents.get(updatedEventsIdx);
+      if (event.getWhen().overlaps(prevEvent.getWhen())) { // If overlaps, replace the current prevEvent, take the later ending time.
+        int laterTiming = 0;
+        if (prevEvent.getWhen().end() > event.getWhen().end()) {
+          laterTiming = prevEvent.getWhen().end();
+        } else {
+          laterTiming = event.getWhen().end();
+        }
+        
+        TimeRange newTimeRange = TimeRange.fromStartEnd(prevEvent.getWhen().start(), laterTiming, false);
+        updatedEvents.set(updatedEventsIdx, new Event("", newTimeRange, Collections.emptySet()));
+      } else {
+        updatedEvents.add(event);
+        updatedEventsIdx++;
+      }      
+    }
+
+    int eventIdx = 0;
+    Event event = updatedEvents.get(eventIdx);
+    int currStartingTime = TimeRange.START_OF_DAY;
+    int currEndingTime = event.getWhen().start();
+    while (currStartingTime < TimeRange.END_OF_DAY && currEndingTime <= TimeRange.END_OF_DAY) {
+      int dur = currEndingTime - currStartingTime;
+      if (dur >= meetingDuration) {
+        boolean inclusive = false;
+        if (currEndingTime == TimeRange.END_OF_DAY) {
+          inclusive = true;
+        }
+        TimeRange timeRange = TimeRange.fromStartEnd(currStartingTime, currEndingTime, inclusive);
+        slotsAvailable.add(timeRange);
+      }
+
+      currStartingTime = updatedEvents.get(eventIdx).getWhen().end();
+      if (currEndingTime == TimeRange.END_OF_DAY) {
+        break;
+      } else if (eventIdx == events.size() - 1) {
+        currEndingTime = TimeRange.END_OF_DAY;
+      } else {
+        eventIdx++;
+        currEndingTime = updatedEvents.get(eventIdx).getWhen().start();
+      }
+    }
+
+    // final int TIME_0830AM = TimeRange.getTimeInMinutes(8, 30);
+    // final int TIME_0900AM = TimeRange.getTimeInMinutes(9, 0);
+    // Collection<TimeRange> expected =
+    //     Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0830AM, false),
+    //         TimeRange.fromStartEnd(TIME_0900AM, TimeRange.END_OF_DAY, true));
+    // return expected;
+
+    return slotsAvailable;
+    
   }
 }
